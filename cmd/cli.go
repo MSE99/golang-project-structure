0eecb2f86e69de1b5e53eae1bd9651af1c110e20
mse99 <mohamedsalemedrah@gmail.com>
2025-04-19 17:14:07 +0200

    Move database commands into their own file under `cmd` pkg

--- cmd/cli.go
+++ cmd/cli.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/mse99/golang-project-structure/config"
-	"github.com/mse99/golang-project-structure/database"
 	"github.com/urfave/cli/v3"
 )
 
@@ -27,30 +25,6 @@ func createVersionCommand() *cli.Command {
 		Action: func(ctx context.Context, c *cli.Command) error {
 			fmt.Println("VERSION 1.0")
 			return nil
-		},
-	}
-}
-
-func createDbCommands() *cli.Command {
-	return &cli.Command{
-		Name:        "db",
-		Description: "Commands to control the database",
-		Commands: []*cli.Command{
-			{
-				Name:        "test",
-				Description: "Tests the connection to the database",
-				Action: func(ctx context.Context, c *cli.Command) error {
-					_, err := database.Connect(ctx, config.DatabaseURL)
-					if err != nil {
-						fmt.Println(err)
-						return err
-					}
-
-					fmt.Println("connected to the database successfully")
-
-					return nil
-				},
-			},
 		},
 	}
 }
